fe93637dbf40fd765989ad88c20c0b31490e3f81
vandyckc <cvandyck@uci.edu>
2023-07-21 16:18:09 -0700

    updating

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,87 +1,72 @@
-// Wait for the document to be ready
-$(document).ready(function() {
-    // Initialize the map
-    const map = L.map('map').setView([0, 0], 2); // Default view at [0, 0] with zoom level 2
-  
-    // Add the base layer (you can choose different map providers here)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-  
-    // Function to set the marker size based on the earthquake magnitude
-    function getMarkerSize(magnitude) {
-      return Math.max(4, magnitude * 4);
-    }
-  
-    // Function to set the marker color based on the earthquake depth
-    function getMarkerColor(depth) {
-      const colors = ['#feebe2', '#fbb4b9', '#f768a1', '#c51b8a', '#7a0177'];
-      if (depth < 10) return colors[0];
-      else if (depth < 30) return colors[1];
-      else if (depth < 50) return colors[2];
-      else if (depth < 70) return colors[3];
-      else return colors[4];
-    }
-  
-    // Function to create popups with earthquake information
-    function createPopup(feature, layer) {
-      const properties = feature.properties;
-      const popupContent = `
-        <strong>Location:</strong> ${properties.place}<br>
-        <strong>Magnitude:</strong> ${properties.mag}<br>
-        <strong>Depth:</strong> ${properties.depth} km<br>
-        <strong>Date:</strong> ${new Date(properties.time)}
-      `;
-      layer.bindPopup(popupContent);
-    }
-  
-    // Function to create the circle markers
-    function createCircleMarker(feature, latlng) {
-      const properties = feature.properties;
-      const markerOptions = {
-        radius: getMarkerSize(properties.mag),
-        fillColor: getMarkerColor(properties.depth),
-        color: '#000',
+// Fetch earthquake data
+fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson")
+  .then(response => response.json())
+  .then(data => {
+    // Create the map centered on a specific location (e.g., San Francisco, CA)
+    const map = L.map("map").setView([37.7749, -122.4194], 6);
+    
+    // Add the tile layer to the map (using OpenStreetMap as the base map)
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+    // Define function to set marker style based on magnitude and depth
+    function getMarkerStyle(magnitude, depth) {
+      return {
+        radius: magnitude * 5, // Adjust the multiplier to control marker size based on magnitude
+        fillColor: getColor(depth),
+        color: "#000",
         weight: 1,
         opacity: 1,
-        fillOpacity: 0.8,
+        fillOpacity: 0.8
       };
-      return L.circleMarker(latlng, markerOptions);
     }
-  
-    // URL to the earthquake data
-    const earthquakeDataURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-  
-    // Fetch the earthquake data using jQuery AJAX
-    $.getJSON(earthquakeDataURL, function(data) {
-      // Create a GeoJSON layer with the earthquake data
-      const earthquakesLayer = L.geoJSON(data, {
-        pointToLayer: createCircleMarker,
-        onEachFeature: createPopup,
-      });
-  
-      // Add the GeoJSON layer to the map
-      earthquakesLayer.addTo(map);
-  
-      // Create a legend
-      const legend = L.control({ position: 'bottomright' });
-  
-      legend.onAdd = function() {
-        const div = L.DomUtil.create('div', 'info legend');
-        const depths = [0, 10, 30, 50, 70];
-        const labels = [];
-  
-        for (let i = 0; i < depths.length; i++) {
-          labels.push(
-            '<i style="background:' + getMarkerColor(depths[i]) + '"></i> ' +
-            depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + ' km' : '+ km')
-          );
-        }
-  
-        div.innerHTML = labels.join('<br>');
-        return div;
-      };
-  
-      // Add the legend to the map
-      legend.addTo(map);
+
+    // Define function to get color based on earthquake depth
+    function getColor(depth) {
+      // You can customize the colors here
+      // For example, let's use green to red gradient for the depth
+      const minDepth = 0;
+      const maxDepth = 90;
+      const gradient = (depth - minDepth) / (maxDepth - minDepth);
+      const startColor = "#00FF00"; // Green
+      const endColor = "#FF0000";   // Red
+
+      // Calculate the color in the gradient based on the depth
+      const r = Math.floor(parseInt(startColor.slice(1, 3), 16) + gradient * (parseInt(endColor.slice(1, 3), 16) - parseInt(startColor.slice(1, 3), 16)));
+      const g = Math.floor(parseInt(startColor.slice(3, 5), 16) + gradient * (parseInt(endColor.slice(3, 5), 16) - parseInt(startColor.slice(3, 5), 16)));
+      const b = Math.floor(parseInt(startColor.slice(5, 7), 16) + gradient * (parseInt(endColor.slice(5, 7), 16) - parseInt(startColor.slice(5, 7), 16)));
+
+      return `rgb(${r}, ${g}, ${b})`;
+    }
+
+    // Loop through the features and add markers to the map
+    data.features.forEach(feature => {
+      const { coordinates } = feature.geometry;
+      const [longitude, latitude, depth] = coordinates;
+      const { mag, place, time, url, title } = feature.properties;
+
+      const marker = L.circleMarker([latitude, longitude], getMarkerStyle(mag, depth))
+        .addTo(map)
+        .bindPopup(`<b>${title}</b><br>Magnitude: ${mag}<br>Depth: ${depth} km<br>Place: ${place}<br><a href="${url}" target="_blank">More info</a>`);
     });
+
+    // Create a legend for earthquake depth
+    const legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function(map) {
+      const div = L.DomUtil.create("div", "info legend");
+      const depths = [0, 50, 70, 90];
+      const colors = ["#00FF00", "#FFFF00", "#FFA500", "#FF0000"];
+
+      div.innerHTML += "<h4>Depth</h4>";
+      for (let i = 0; i < depths.length; i++) {
+        div.innerHTML +=
+          '<i style="background:' + colors[i] + '"></i> ' +
+          depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + ' km<br>' : '+ km');
+      }
+
+      return div;
+    };
+
+    // Add the legend to the map
+    legend.addTo(map);
   });
-  
